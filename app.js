3f71ca0cdcecb5c9870eb9336433b23c625bfe34
Ehan <admin@admins-iMac.local>
2024-12-21 00:09:17 +0000

    create user added

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const {
   commentPost,
   updateArticleVotes,
   deleteComment,
-  getUsers
+  getUsers,
+  addUser,
 } = require("./controller");
 const {errorHandler} =  require('./error')
 const app = express();
@@ -22,7 +23,8 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleId);
 app.get("/api/articles/:article_id/comments",getComments);
-app.post('/api/articles/:article_id/comments',commentPost)
+app.post('/api/articles/:article_id/comments',commentPost);
+app.post('/api/users/adduser',addUser)
 app.patch('/api/articles/:article_id', updateArticleVotes);
 app.delete('/api/comments/:comment_id',deleteComment)
 app.get("/api/users", getUsers);
