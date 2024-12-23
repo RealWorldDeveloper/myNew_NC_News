af3002ef042d80d6e477769fa755882096d3ae53
Ehan <admin@admins-iMac.local>
2024-12-23 15:30:24 +0000

    JWT Update

--- controller.js
+++ controller.js
@@ -148,7 +148,7 @@ const login = (req, res, next) => {
       } else {
         bcrypt.compare(password, findUser.password).then((matchPass) => {
           if (matchPass) {
-            const token = JWT.sign({ username: findUser.username }, process.env.production.secret_key, { expiresIn: '1h' });
+            const token = JWT.sign({ username: findUser.username }, process.env.SECRET_KEY, { expiresIn: '1h' });
 
             // Set the token as a cookie
             res.cookie('token', token, {
