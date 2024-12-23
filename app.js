ed51d3e73f8d7d0a8e6e46a4112f07ba58da53f1
Ehan <admin@admins-iMac.local>
2024-12-23 11:39:09 +0000

    JWT REMOVED

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const {errorHandler} =  require('./error')
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:['http://localhost:5173'],
   credentials:true
 }));
 app.use(cookie())
